830415624905995e8909f7d9e7e4f28a08609aa6
Prakriti Pandey <prakritip729@gmail.com>
2026-02-10 18:32:16 +0000

    conversion of operator

---
+++ 01_basics/convertion_operator.js
@@ -0,0 +1,30 @@
+let score = "223hgfg"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);  
+console.log(valueInNumber)
+
+
+//"234" <= 234
+//"3434fgfds"<= NaN
+// true <= 1; false<=0
+//undefined <= undefined
+//null <= NaN
+
+let isLoggedIn = "prakriti"
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 <= true ; 0 <= false
+// "" <= false
+//"prakriti" <= true
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
