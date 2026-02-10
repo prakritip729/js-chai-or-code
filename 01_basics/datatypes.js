738b7da01533a8f8c8a9da5671e926a8c6d4f2ad
Prakriti Pandey <prakritip729@gmail.com>
2026-02-10 06:55:05 +0000

    commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";       // treat all JS code as newer version//
+
+// alert(3 + 3 ) // we are using nodejs,not browser//
+
+console.log(3 + 3 )
+console.log("prakriti")
+
+
+let name = "prakriti"
+let age = 20
+let isloggedIn = true
+let state;
+
+
+//number => 2 to the power 53
+//Bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol
+
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof null)             //object
+console.log(typeof undefined)         //undefined
