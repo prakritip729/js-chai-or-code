8a163e8c6f357184612035f052707150d29bf3ed
Prakriti Pandey <prakritip729@gmail.com>
2026-02-11 14:22:29 +0000

    operators

---
+++ 01_basics/operator.js
@@ -0,0 +1,35 @@
+let value = 66
+let negValue = -value
+console.log(negValue);     //used to give negative value convert to negative value 
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " prakriti"
+console.log(str1 +str2)
+
+console.log("1" + 2);         //12
+console.log(1 + "2");         //12 
+console.log("1" + "2");       //12  
+console.log("1" + 2 + 2);      //122 
+console.log((1 + 2) + "3");     //33  
+
+
+console.log(+true)
+console.log(+" ")     //1 
+
+
+let sum = 23
+++sum;
+console.log(sum);
+
+
+//link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.
+//html#sec-type-conversion
