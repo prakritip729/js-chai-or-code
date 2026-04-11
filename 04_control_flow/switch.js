3426a9f512611a2e1a67f2531f39718ac2895ac5
Prakriti Pandey <prakritip729@gmail.com>
2026-04-11 03:19:33 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+//  switch(key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+//  }
+ 
+const month = 3
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april");
+        break;    
+    default:
+        console.log("default case match");
+        break;
+}
+
+//if we want to add string we can add string value in place of numbers like 
+// const months = "march"
+// switch(months){
+//     case "jan":
+//         console.log("jan");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march")
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;    
+//     default:
+//         console.log("default case match");
+//         break;
+// }
